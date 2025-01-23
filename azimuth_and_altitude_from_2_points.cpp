83724b32e3b55da6029f10b1e557a830428a214b
Mårten Nettelbladt <56385093+martennettelbladt@users.noreply.github.com>
2025-01-23 13:40:03 +0100

    Update azimuth_and_altitude_from_2_points.cpp

--- azimuth_and_altitude_from_2_points.cpp
+++ azimuth_and_altitude_from_2_points.cpp
@@ -4,10 +4,10 @@
 // Azimuth from two 2D points. First point is origin
 // Result as an angle in radians. 0 = X-axis. Full circle = 6.28 radians
 
-float getAzimuth(x1, y1, x2, y2)
+float getAzimuth(x_1, y_1, x_2, y_2)
 {
-	x = x2 - x1;
-	y = y2 - y1;
+	x = x_2 - x_1;
+	y = y_2 - y_1;
 	float azimuth = atanf(abs(y) / abs(x));
 	if (x < 0) azimuth = M_PI - azimuth;
 	if (y < 0) azimuth = 2 * M_PI - azimuth;
@@ -18,11 +18,11 @@ float getAzimuth(x1, y1, x2, y2)
 // Altitude from two 3D points. First point is origin.
 // Result as an angle in radians. 0 = Horizon, 3.14 = up, -3.14 = down.
 
-float getAltitude(x1, y1, z1, x2, y2, z2)
+float getAltitude(x_1, y_1, z_1, x_2, y_2, z_2)
 {
-	x = x2 - x1;
-	y = y2 - y1;
-	z = z2 - z1;
+	x = x_2 - x_1;
+	y = y_2 - y_1;
+	z = z_2 - z_1;
 	float distanceXY = sqrt(pow(x, 2) + pow(y, 2));
 	float altitude = atanf(abs(z) / distanceXY);
 	if (z < 0) altitude = 0 - altitude;
