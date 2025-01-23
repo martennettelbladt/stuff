ca4c8c6dbe9cbfecbc89f88ae289d79ff82df692
Mårten Nettelbladt <56385093+martennettelbladt@users.noreply.github.com>
2025-01-23 14:47:47 +0100

    Update azimuth_and_altitude_from_2_points.cpp

--- azimuth_and_altitude_from_2_points.cpp
+++ azimuth_and_altitude_from_2_points.cpp
@@ -2,29 +2,46 @@
 
 
 // Azimuth from two 2D points. First point is origin
-// Result as an angle in radians. 0 = X-axis. Full circle = 6.28 radians
+// Returns valus in the range [-pi, pi]
 
-float getAzimuth(X1, Y1, X2, Y2)
+#include <math.h>
+
+float getAzimuth(float X1, float Y1, float X2, float Y2)
 {
-	x = X2 - X1;
-	y = Y2 - Y1;
-	float azimuth = atanf(abs(y) / abs(x));
-	if (x < 0) azimuth = M_PI - azimuth;
-	if (y < 0) azimuth = 2 * M_PI - azimuth;
+	float x = X2 - X1;
+	float y = Y2 - Y1;
+
+	// Handle special cases where x is 0
+	if (x == 0)
+	{
+		if (y > 0) return M_PI_2; // 90 degrees
+		if (y < 0) return -M_PI_2; // -90 degrees
+		return 0; // exactly at origin
+	}
+	
+	float azimuth = atan2(y, x); // atan2 handles quadrants, returns valus in the range [-pi, pi]
 	return azimuth;
 }
 
 
 // Altitude from two 3D points. First point is origin.
-// Result as an angle in radians. 0 = Horizon, 3.14 = up, -3.14 = down.
+// Result as an angle in radians. 0 = Horizon, pi/2 = up, pi/2 = down.
 
-float getAltitude(X1, Y1, Z1, X2, Y2, Z2)
+float getAltitude(float X1, float Y1, float Z1, float X2, float Y2, float Z2)
 {
-	x = X2 - X1;
-	y = Y2 - Y1;
-	z = Z2 - Z1;
-	float distanceXY = sqrt(pow(x, 2) + pow(y, 2));
-	float altitude = atanf(abs(z) / distanceXY);
-	if (z < 0) altitude = 0 - altitude;
+	float x = X2 - X1;
+	float y = Y2 - Y1;
+	float z = Z2 - Z1;
+
+	float distanceXY = sqrt(x * x + y * y);
+
+	// Handle special case where distanceXY is 0
+	if (distanceXY == 0)
+	{
+		if (z > 0) return M_PI_2; // 90 degrees = straight up
+		if (z < 0) return -M_PI_2; // -90 degrees = straight down
+		return 0; // exactly at origin
+		
+	float altitude = atan2(z, distanceXY);
 	return altitude;
 }
