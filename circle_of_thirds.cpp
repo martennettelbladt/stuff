06452af983c805ea5a797aa68dbf0a188ae47cf0
Mårten Nettelbladt <56385093+martennettelbladt@users.noreply.github.com>
2025-01-29 09:28:42 +0100

    Update circle_of_thirds.cpp

--- circle_of_thirds.cpp
+++ circle_of_thirds.cpp
@@ -13,3 +13,10 @@ int main()
         std::cout << note[i] << "\n";
     }
 }
+
+/*
+result:
+0, 3, 7, 10, 2, 5, 9, 0, 4, 7, 11, 2, 6, 9, 1, 4, 8, 11, 3, 6, 10, 1, 5, 8
+three consecutive numbers form a minor or major triad
+the numbers indicate which note number of the 12 semitones in a chromatic scale, 0-11
+*/
