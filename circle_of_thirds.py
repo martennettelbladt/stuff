bbc31f0763e912366578aba21e1708961a81d1e0
Mårten Nettelbladt <56385093+martennettelbladt@users.noreply.github.com>
2025-01-29 09:28:12 +0100

    Update circle_of_thirds.py

--- circle_of_thirds.py
+++ circle_of_thirds.py
@@ -8,3 +8,8 @@ note = []
 for i in range(24):
     note.append(math.floor(3.5 * i) % 12)
     print(note[i])
+
+# result:
+# 0, 3, 7, 10, 2, 5, 9, 0, 4, 7, 11, 2, 6, 9, 1, 4, 8, 11, 3, 6, 10, 1, 5, 8
+# three consecutive numbers form a minor or major triad
+# the numbers indicate which note number of the 12 semitones in a chromatic scale, 0-11
